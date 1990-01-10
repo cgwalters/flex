7fb5699c0f4e481e6a8c9d6827730de316420b22
vern <vern>
1990-01-10 15:10:22 +0000

    Initial revision

---
+++ libmain.c
@@ -0,0 +1,9 @@
+/* libmain - flex run-time support library "main" function */
+
+/* $Header: /cvsroot/flex/flex/libmain.c,v 1.1 1990/01/10 15:10:22 vern Exp $ */
+
+main()
+
+    {
+    yylex();
+    }
