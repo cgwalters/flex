d79a9f46ce340ca2c8a0ca2686bff0a1dfb5dde1
vern <vern>
1990-05-26 16:50:08 +0000

    Added declaration of arguments made yylex() a function

--- libmain.c
+++ libmain.c
@@ -1,9 +1,13 @@
 /* libmain - flex run-time support library "main" function */
 
-/* $Header: /cvsroot/flex/flex/libmain.c,v 1.1 1990/01/10 15:10:22 vern Exp $ */
+/* $Header: /cvsroot/flex/flex/libmain.c,v 1.2 1990/05/26 16:50:08 vern Exp $ */
 
-main()
+extern int yylex();
+
+int main( argc, argv )
+int argc;
+char *argv[];
 
     {
-    yylex();
+    return yylex();
     }
