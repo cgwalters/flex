818f6720384b51feebae6f3b54ec7e68ba10661d
vern <vern>
1993-04-14 22:41:35 +0000

    Reformatting.

--- libmain.c
+++ libmain.c
@@ -1,13 +1,12 @@
 /* libmain - flex run-time support library "main" function */
 
-/* $Header: /cvsroot/flex/flex/libmain.c,v 1.2 1990/05/26 16:50:08 vern Exp $ */
+/* $Header: /cvsroot/flex/flex/libmain.c,v 1.3 1993/04/14 22:41:55 vern Exp $ */
 
 extern int yylex();
 
 int main( argc, argv )
 int argc;
 char *argv[];
-
-    {
-    return yylex();
-    }
+	{
+	return yylex();
+	}
