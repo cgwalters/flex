1ee751ab6fda9fadd0564a0ca154219cbb444b43
vern <vern>
1995-01-09 21:02:03 +0000

    better fix for #pragma portability problem

--- flexdef.h
+++ flexdef.h
@@ -26,34 +26,25 @@
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-/* @(#) $Header: /cvsroot/flex/flex/flexdef.h,v 2.47 1995/01/09 12:41:12 vern Exp $ (LBL) */
+/* @(#) $Header: /cvsroot/flex/flex/flexdef.h,v 2.48 1995/01/09 21:02:03 vern Exp $ (LBL) */
 
 #include <stdio.h>
 #include <ctype.h>
 
 #include "config.h"
 
-#ifndef __TURBOC__
-/* Some C preprocessors barf when they see the following #pragma's,
- * even though __TURBOC__ isn't defined so they should just skip them.
- * So we comment them out by leaving this comment
- open
-#endif
 #ifdef __TURBOC__
 #define HAVE_STRING_H 1
 #define MS_DOS 1
 #ifndef __STDC__
 #define __STDC__ 1
 #endif
-#pragma warn -pro
-#pragma warn -rch
-#pragma warn -use
-#pragma warn -aus
-#pragma warn -par
-#pragma warn -pia
-#endif
-#ifndef __TURBOC__
- close open comment above */
+ #pragma warn -pro
+ #pragma warn -rch
+ #pragma warn -use
+ #pragma warn -aus
+ #pragma warn -par
+ #pragma warn -pia
 #endif
 
 #ifdef HAVE_STRING_H
